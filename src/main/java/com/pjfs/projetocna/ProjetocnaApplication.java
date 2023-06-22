03f798e0c1b02fd02224452d94d3b0dfabc499e8
Rafael Silva <rafa.306@hotmail.com>
2023-06-22 17:29:34 -0300

    Projeto criado

---
+++ src/main/java/com/pjfs/projetocna/ProjetocnaApplication.java
@@ -0,0 +1,13 @@
+package com.pjfs.projetocna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjfs/projetocna/ProjetocnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjfs.projetocna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
