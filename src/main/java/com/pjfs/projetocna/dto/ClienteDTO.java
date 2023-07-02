7f90290a77598b425aa69eebd2b1080731fd7993
Rafael <rafa.306@hotmail.com>
2023-07-02 10:54:29 -0300

    Validação custumizada, validação na atualização de email de cleinte

--- src/main/java/com/pjfs/projetocna/dto/ClienteDTO.java
+++ src/main/java/com/pjfs/projetocna/dto/ClienteDTO.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 import org.hibernate.validator.constraints.Length;
 
 import com.pjfs.projetocna.domain.Cliente;
+import com.pjfs.projetocna.services.validation.ClienteUpdate;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable{
 
     private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/pjfs/projetocna/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.pjfs.projetocna.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.pjfs.projetocna.domain.Cliente;
+import com.pjfs.projetocna.dto.ClienteDTO;
+import com.pjfs.projetocna.repositories.ClienteRepository;
+import com.pjfs.projetocna.resources.exception.FieldMessage;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+				
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+			
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
