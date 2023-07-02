9fa027003cfec9ee86a0420105739007ea7c126b
Rafael <rafa.306@hotmail.com>
2023-07-02 10:33:04 -0300

    Adicionada validação de CPF e CNPJ para a entidade Cliente

---
+++ src/main/java/com/pjfs/projetocna/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.pjfs.projetocna.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pjfs.projetocna.domain.enums.TipoCliente;
+import com.pjfs.projetocna.dto.ClienteNewDTO;
+import com.pjfs.projetocna.resources.exception.FieldMessage;
+import com.pjfs.projetocna.services.validation.utils.BR;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
